a81c6f0c666e3c833594f5892c9119a671f393db
dhruvtrip <dhruvtripathi1995@gmail.com>
2022-03-09 00:39:43 +0100

    implement calculate winner function

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-board',
@@ -40,7 +39,28 @@ export class BoardComponent implements OnInit {
   }
 
   calculateWinner(){
-    return '';
+    const winningLines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,4,8],
+      [2,4,6],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8]
+    ];
+
+    for (let i = 0; i < winningLines.length; i++) {
+      const [a,b,c] = winningLines[i];
+      if(
+        this.squares[a] && 
+        this.squares[a] === this.squares[b] && 
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
   }
 
 }
